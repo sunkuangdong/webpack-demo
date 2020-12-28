420faae9004831c06479b02c226cc5a870abeca6
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-12-28 16:03:36 +0800

    updata

--- src/x.js
+++ src/x.js
@@ -1,1 +1,2 @@
+import './x.css'
 export default "222222";
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
+        path: path.resolve(__dirname, './dist'),
         filename: '[name].[contenthash].js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: "Webpack 学习",
+        template: "./src/assets/index.html"
+    })],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }, ],
+    },
 };
